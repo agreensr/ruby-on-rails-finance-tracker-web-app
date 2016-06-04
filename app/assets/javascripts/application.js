8f6df04a3304a337cf34492ea133ff65829bc005
cobra390hp1 <antoinegreensr@gmail.com>
2016-06-04 15:51:30 -0500

    assets precompiled locally

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-
+// require turbolinks
 //= require_tree .
 
 var hide_spinner = function(){
